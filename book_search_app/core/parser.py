22159cdf4882a19a807686344be6ef0a6f762cfc
Anand Sengar <gitu.daas@gmail.com>
2020-04-27 10:34:49 +0530

    Add Logic to Sanitize Summaries

--- book_search_app/core/parser.py
+++ book_search_app/core/parser.py
@@ -7,9 +7,10 @@ from collections import defaultdict
 from book_search_app.utils.helper import filter_stopwords, find_indexes
 
 
-def file_parser(filepath: str) -> dict:
+def file_parser(filepath: str) -> (dict, dict):
     """
     Takes the file path to read for parsing and returns the invert index of the data.
+    Also, returns the summary_id mapping to actual summary.
 
     :param filepath:
     :return:
@@ -19,11 +20,29 @@ def file_parser(filepath: str) -> dict:
 
     indexed_map = defaultdict(list)
     for summary_item in filedata['summaries']:
-        summary_item['summary'] = summary_item['summary'].replace("\xa0", " ")
+        # Sanitize the data
+        summary_item['summary'] = sanitize_data(summary_item['summary'])
+
         word_list = filter_stopwords(summary_item['summary'].split())
+        processed_words = set()
         for word in word_list:
-            indexed_map[word].append([summary_item['id'], find_indexes(word_list, word)])
+            if word not in processed_words:
+                indexed_map[word].append([summary_item['id'], find_indexes(word_list, word)])
+                processed_words.add(word)
 
-    return indexed_map
+    summary_map = {summary['id']: {'summary': summary['summary']} for summary in filedata['summaries']}
+    return indexed_map, summary_map
 
 
+def sanitize_data(sentence):
+    """
+    Removes replaces the unwanted characters
+
+    :param sentence:
+    :return:
+    """
+    sentence = sentence.replace("\xa0", " ")
+    sentence = sentence.replace("\u00a0", " ")
+    sentence = sentence.replace("The Book in Three Sentences: ", "")
+    sentence = sentence.replace("\u201c", '"').replace("\u201d", '"')
+    return sentence

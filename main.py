aee4506e732493352ec0fba8de31c66fbc27b3da
Anand Sengar <gitu.daas@gmail.com>
2020-04-30 12:25:19 +0530

    Take Number of Results to output as input

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Main module to use the functionality using manual command input.
 """
-from book_search_app.core.parser import file_parser
+from book_search_app.core.parser import file_parser, sanitize_data
 from book_search_app.core.query import search_query
 
 
@@ -10,8 +10,9 @@ if __name__ == '__main__':
     indexed_map, summary_map = file_parser(filepath=filename)
     while True:
         query = input("Provide the query to search: ")
-        k = 5
+        k = int(input("How many max results you want: "))
         doc_ids = search_query(query, indexed_map)
         print(doc_ids)
-        for item in doc_ids[:5]:
-            print(summary_map[item])
+        for item in doc_ids[:k]:
+            summary_map[item]['summary'] = sanitize_data(summary_map[item]['summary'])
+            print(summary_map[item]['summary'])
